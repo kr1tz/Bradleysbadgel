e449f59a8b2cc0de2fe3f48a6128b83be9d4f18b
Kris Trinidad <kristrinidad@gmail.com>
2019-01-21 15:07:29 -0600

    Initial commit

---
+++ BradleysBagel/WindowsApplication1/BradleysBagel.Designer.vb
@@ -0,0 +1,203 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Me.MenuStrip = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PrintBagelListToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveBagelListToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AddBagelTypeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RemoveBagelTypeToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ClearBagelListToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.DisplayToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BagelLabel = New System.Windows.Forms.Label()
+        Me.BagelComboBox = New System.Windows.Forms.ComboBox()
+        Me.PrintDocument = New System.Drawing.Printing.PrintDocument()
+        Me.PrintPreviewDialog = New System.Windows.Forms.PrintPreviewDialog()
+        Me.MenuStrip.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip
+        '
+        Me.MenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.EditToolStripMenuItem, Me.HelpToolStripMenuItem})
+        Me.MenuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip.Name = "MenuStrip"
+        Me.MenuStrip.Size = New System.Drawing.Size(288, 24)
+        Me.MenuStrip.TabIndex = 1
+        Me.MenuStrip.Text = "MenuStrip"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PrintBagelListToolStripMenuItem, Me.SaveBagelListToolStripMenuItem, Me.ToolStripSeparator1, Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'PrintBagelListToolStripMenuItem
+        '
+        Me.PrintBagelListToolStripMenuItem.Name = "PrintBagelListToolStripMenuItem"
+        Me.PrintBagelListToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.PrintBagelListToolStripMenuItem.Text = "Print Bagel List"
+        '
+        'SaveBagelListToolStripMenuItem
+        '
+        Me.SaveBagelListToolStripMenuItem.Name = "SaveBagelListToolStripMenuItem"
+        Me.SaveBagelListToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.SaveBagelListToolStripMenuItem.Text = "Save Bagel List"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(149, 6)
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'EditToolStripMenuItem
+        '
+        Me.EditToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AddBagelTypeToolStripMenuItem, Me.RemoveBagelTypeToolStripMenuItem, Me.ClearBagelListToolStripMenuItem, Me.ToolStripSeparator2, Me.DisplayToolStripMenuItem})
+        Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
+        Me.EditToolStripMenuItem.Size = New System.Drawing.Size(39, 20)
+        Me.EditToolStripMenuItem.Text = "&Edit"
+        '
+        'AddBagelTypeToolStripMenuItem
+        '
+        Me.AddBagelTypeToolStripMenuItem.Name = "AddBagelTypeToolStripMenuItem"
+        Me.AddBagelTypeToolStripMenuItem.ShortcutKeyDisplayString = "CTRL+A"
+        Me.AddBagelTypeToolStripMenuItem.Size = New System.Drawing.Size(227, 22)
+        Me.AddBagelTypeToolStripMenuItem.Text = "Add Bagel Type"
+        '
+        'RemoveBagelTypeToolStripMenuItem
+        '
+        Me.RemoveBagelTypeToolStripMenuItem.Name = "RemoveBagelTypeToolStripMenuItem"
+        Me.RemoveBagelTypeToolStripMenuItem.ShortcutKeyDisplayString = "CTRL+R"
+        Me.RemoveBagelTypeToolStripMenuItem.Size = New System.Drawing.Size(227, 22)
+        Me.RemoveBagelTypeToolStripMenuItem.Text = "Remove Bagel Type"
+        '
+        'ClearBagelListToolStripMenuItem
+        '
+        Me.ClearBagelListToolStripMenuItem.Name = "ClearBagelListToolStripMenuItem"
+        Me.ClearBagelListToolStripMenuItem.ShortcutKeyDisplayString = "F3"
+        Me.ClearBagelListToolStripMenuItem.Size = New System.Drawing.Size(227, 22)
+        Me.ClearBagelListToolStripMenuItem.Text = "Clear Bagel List"
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(224, 6)
+        '
+        'DisplayToolStripMenuItem
+        '
+        Me.DisplayToolStripMenuItem.Name = "DisplayToolStripMenuItem"
+        Me.DisplayToolStripMenuItem.Size = New System.Drawing.Size(227, 22)
+        Me.DisplayToolStripMenuItem.Text = "Display Bagel Type Count"
+        '
+        'HelpToolStripMenuItem
+        '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem})
+        Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
+        Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.HelpToolStripMenuItem.Text = "&Help"
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(107, 22)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'BagelLabel
+        '
+        Me.BagelLabel.AutoSize = True
+        Me.BagelLabel.Location = New System.Drawing.Point(87, 56)
+        Me.BagelLabel.Name = "BagelLabel"
+        Me.BagelLabel.Size = New System.Drawing.Size(73, 13)
+        Me.BagelLabel.TabIndex = 2
+        Me.BagelLabel.Text = "Type of &Bagel"
+        '
+        'BagelComboBox
+        '
+        Me.BagelComboBox.FormattingEnabled = True
+        Me.BagelComboBox.Location = New System.Drawing.Point(90, 72)
+        Me.BagelComboBox.Name = "BagelComboBox"
+        Me.BagelComboBox.Size = New System.Drawing.Size(121, 21)
+        Me.BagelComboBox.TabIndex = 3
+        '
+        'PrintDocument
+        '
+        '
+        'PrintPreviewDialog
+        '
+        Me.PrintPreviewDialog.AutoScrollMargin = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog.AutoScrollMinSize = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog.ClientSize = New System.Drawing.Size(400, 300)
+        Me.PrintPreviewDialog.Enabled = True
+        Me.PrintPreviewDialog.Icon = CType(resources.GetObject("PrintPreviewDialog.Icon"), System.Drawing.Icon)
+        Me.PrintPreviewDialog.Name = "PrintPreviewDialog"
+        Me.PrintPreviewDialog.Visible = False
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(288, 164)
+        Me.Controls.Add(Me.BagelComboBox)
+        Me.Controls.Add(Me.BagelLabel)
+        Me.Controls.Add(Me.MenuStrip)
+        Me.Name = "MainForm"
+        Me.Text = "Bradley's Bagel"
+        Me.MenuStrip.ResumeLayout(False)
+        Me.MenuStrip.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PrintBagelListToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SaveBagelListToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents ExitToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents EditToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AddBagelTypeToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents RemoveBagelTypeToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ClearBagelListToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator2 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents DisplayToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HelpToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AboutToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents BagelLabel As System.Windows.Forms.Label
+    Friend WithEvents BagelComboBox As System.Windows.Forms.ComboBox
+    Friend WithEvents PrintDocument As System.Drawing.Printing.PrintDocument
+    Friend WithEvents PrintPreviewDialog As System.Windows.Forms.PrintPreviewDialog
+
+End Class
---
+++ BradleysBagel/WindowsApplication1/BradleysBagel.vb
@@ -0,0 +1,220 @@
+﻿Imports Microsoft.VisualBasic.FileIO
+Imports System.IO
+Public Class MainForm
+
+    Private IsDirtyBoolean As Boolean
+
+    Private Sub MainForm_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+
+        Dim ResponseDialogResult As DialogResult
+        Dim BagelTypeString As String
+
+        Try
+            'Open the file
+
+            Dim TypesStreamReader As StreamReader = New StreamReader("BagelType.txt")
+
+            'Read all element into the list
+
+            Do Until TypesStreamReader.Peek = -1
+                BagelTypeString = TypesStreamReader.ReadLine()
+                BagelComboBox.Items.Add(BagelTypeString)
+
+            Loop
+            'Close the file
+            TypesStreamReader.Close()
+
+        Catch ex As Exception
+            'File missing
+
+            ResponseDialogResult = MessageBox.Show("Create a new file?", "File Not Found", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+            If ResponseDialogResult = DialogResult.No Then
+                'Exit Program
+                Me.Close()
+            End If
+
+        End Try
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles ExitToolStripMenuItem.Click
+        'Close the program
+        Me.Close()
+    End Sub
+
+
+    Private Sub AddBagelTypeToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles AddBagelTypeToolStripMenuItem.Click
+        'Add a new bagel to the Bagel list
+        With BagelComboBox
+            'Test for blank input
+            If .Text <> "" Then
+                'Make sure the item is not already on the list
+                Dim ItemFoundBoolean As Boolean
+                Dim ItemIndexInteger As Integer
+                Do Until ItemFoundBoolean Or ItemIndexInteger = .Items.Count
+                    If .Text = .Items(ItemIndexInteger).ToString() Then
+                        ItemFoundBoolean = True
+                        Exit Do
+                    Else
+                        ItemIndexInteger += 1
+                    End If
+                Loop
+                If ItemFoundBoolean Then
+                    MessageBox.Show("Duplicate item.", "Add Failed", _
+                    MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                Else
+                    'If its not in the list, add it
+                    .Items.Add(.Text)
+                    .Text = ""
+                End If
+            Else
+                MessageBox.Show("Enter a Bagel type to add", "Missing Data", MessageBoxButtons.OK, _
+                                MessageBoxIcon.Exclamation)
+            End If
+
+            'Sort the list alphabetically
+            BagelComboBox.Sorted = True
+
+            .Focus()
+        End With
+    End Sub
+    Private Sub SaveBagelListToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles SaveBagelListToolStripMenuItem.Click
+
+        'Saves the bagel list in a file
+
+        Dim NumberItemsInteger As Integer
+
+        'Open the file
+        Dim TypesStreamWriter As New StreamWriter("BagelType.txt", False)
+
+        'Saves items in the file
+
+        NumberItemsInteger = BagelComboBox.Items.Count - 1
+        For IndexInteger As Integer = 0 To NumberItemsInteger
+            TypesStreamWriter.WriteLine(BagelComboBox.Items(IndexInteger))
+
+        Next IndexInteger
+        TypesStreamWriter.Close()
+        IsDirtyBoolean = False
+
+
+    End Sub
+
+
+    Private Sub RemoveBagelTypeToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles RemoveBagelTypeToolStripMenuItem.Click
+        'Remove the selected Bagel tpe from the list
+
+        With BagelComboBox
+            If .SelectedIndex <> -1 Then
+                .Items.RemoveAt(.SelectedIndex)
+            Else
+                MessageBox.Show("First select the Bagel you want to remove from the list", "No selection made", MessageBoxButtons.OK, _
+                MessageBoxIcon.Exclamation)
+            End If
+        End With
+
+        'Sort the list alphabetically
+        BagelComboBox.Sorted = True
+    End Sub
+
+    Private Sub ClearBagelListToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles ClearBagelListToolStripMenuItem.Click
+        Dim ResponseDialogResult As DialogResult
+
+        ResponseDialogResult = MessageBox.Show("Do you want to clear the entire Bagel list?", "Clear Bagel list", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+        If ResponseDialogResult = DialogResult.Yes Then
+            BagelComboBox.Items.Clear()
+
+        End If
+
+    End Sub
+
+    Private Sub DisplayToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles DisplayToolStripMenuItem.Click
+        'Display a count of the different Bagels in the list
+        Dim MessageString As String
+        MessageString = "The differnt number of Bagels on the list is " & _
+        BagelComboBox.Items.Count & "."
+        MessageBox.Show(MessageString, "Bradley's Bagels Type Count", _
+        MessageBoxButtons.OK, MessageBoxIcon.Information)
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles AboutToolStripMenuItem.Click
+
+        AboutBox.ShowDialog()
+
+    End Sub
+
+
+    Private Sub MainForm_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        'Ask the user to save the file
+
+        Dim ResponseDialogResult As DialogResult
+        Dim MessageString As String = "Bagel list has changed. Save the list?"
+
+        If IsDirtyBoolean Then
+            ResponseDialogResult = MessageBox.Show(MessageString, "Bagel List Changed", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+            If ResponseDialogResult = DialogResult.Yes Then
+                SaveBagelListToolStripMenuItem_Click(sender, e)
+            End If
+        End If
+
+
+
+    End Sub
+
+
+
+    Private Sub PrintDocument_PrintPage(sender As System.Object, e As System.Drawing.Printing.PrintPageEventArgs) Handles PrintDocument.PrintPage
+        'Declarations
+        Dim PrintFont As New Font("Arial", 12)
+        Dim LineHightSingle As Single = PrintFont.GetHeight + 2 'Add two pixels for spacing.
+        Dim HorizontalPrintLocationSingle As Single
+        Dim VerticalPrintLocationSingle As Single
+        Dim PrintLineString As String = "This is a Test Line to Print"
+
+        HorizontalPrintLocationSingle = e.MarginBounds.Left
+        VerticalPrintLocationSingle = e.MarginBounds.Top
+
+        If BagelComboBox.Items.Count = 0 Then
+            MsgBox("Please create a Bagel List")
+
+        Else
+            Using centerFormat As New StringFormat(StringFormatFlags.NoClip)
+                With (centerFormat)
+                    .Alignment = StringAlignment.Center
+                    ' .LineAlignment = StringAlignment.Near
+                End With
+
+                Using headerFont As New Font("Times New Roman", 20, FontStyle.Bold Or FontStyle.Underline)
+                    Dim headerString As String = "Bagel Type List"
+                    e.Graphics.DrawString(headerString, headerFont, Brushes.Black, e.MarginBounds, centerFormat)
+                End Using
+            End Using
+
+
+            'Print out all the items in he BagelComboBox list
+            For ListIndexInteger As Integer = 0 To BagelComboBox.Items.Count - 1
+
+                'Set up a line
+                PrintLineString = BagelComboBox.Items(ListIndexInteger).ToString()
+
+                'Send the line to the graphics page object
+                e.Graphics.DrawString(PrintLineString, PrintFont, Brushes.Black, HorizontalPrintLocationSingle, VerticalPrintLocationSingle)
+
+                'Increment the Y position for the next line
+                VerticalPrintLocationSingle += LineHightSingle
+
+            Next ListIndexInteger
+        End If
+
+
+    End Sub
+
+
+    Private Sub PrintBagelListToolStripMenuItem_Click(sender As System.Object, e As System.EventArgs) Handles PrintBagelListToolStripMenuItem.Click
+        PrintPreviewDialog.Document = PrintDocument
+        PrintPreviewDialog.ShowDialog()
+    End Sub
+
+End Class
